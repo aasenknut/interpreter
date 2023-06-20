6ceffb77188e9b7045f0de24d6eb307673c141ce
aasenknut <aasenknut@gmail.com>
2023-06-20 14:55:57 +0200

    (s) readme update

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ This is the *Glox* language.
 To say that this is inspired by *Nystrom, B. (2015), Crafting Interpreters*,
 would be the understatement of the year.
 
+Some notes on the features of the language:
+- *Lexer*. Only single byte characters -- not UTF-8.
+- *Parser*. Recursive decent.
+    - Associates:
+        - *Left*. Equality (`==, !=`), comparison (`>, >=, <, <=`), term (`+, -`), factor (`*, /`).
+        - *Right*. Unary (`!, -`).
+
 ---
 *Powered by technical debt*.
--- parser.go
+++ parser.go
@@ -1,3 +1,17 @@
+/*
+GRAMMAR
+=======
+expression     → equality ;
+equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+term           → factor ( ( "-" | "+" ) factor )* ;
+factor         → unary ( ( "/" | "*" ) unary )* ;
+unary          → ( "!" | "-" ) unary
+               | primary ;
+primary        → NUMBER | STRING | "true" | "false" | "nil"
+               | "(" expression ")" ;
+*/
+
 package main
 
 import "fmt"
